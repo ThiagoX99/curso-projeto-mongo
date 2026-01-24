1d091e2e7cb9ec06eff5656333216bf69e78cc55
Thiago X99 <thiago.souzadasilva9901@gmail.com>
2026-01-24 15:43:47 -0300

    Consulta com vários critérios

--- src/main/java/com/thiago/workshopmongo/services/PostService.java
+++ src/main/java/com/thiago/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.thiago.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.thiago.workshopmongo.domain.Post;
-import com.thiago.workshopmongo.domain.User;
 import com.thiago.workshopmongo.repository.PostRepository;
 import com.thiago.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,5 +25,10 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text){
 		return repo.searcheTitle(text);
+	}
+	
+	public List<Post>  fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24*60*60*1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
